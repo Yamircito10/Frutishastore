7586a6af127579cca8179711078f6cef963f2ef9
Yamircito10 <elpoderoso967@gmail.com>
2025-12-27 18:09:18 -0500

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -1,12 +1,12 @@
 // firebase-init.js (compat)
 // Pega tu firebaseConfig real aquí (Firebase console > Project settings > Your apps)
 const firebaseConfig = {
-  apiKey: "REEMPLAZA_AQUI",
-  authDomain: "REEMPLAZA_AQUI",
-  projectId: "REEMPLAZA_AQUI",
-  storageBucket: "REEMPLAZA_AQUI",
-  messagingSenderId: "REEMPLAZA_AQUI",
-  appId: "REEMPLAZA_AQUI"
+  apiKey: "AIzaSyBtNWOs53cdTm0SYUZe_qCQb4OC-_VdcMQ",
+  authDomain: "inventario-ab270.firebaseapp.com",
+  projectId: "inventario-ab270",
+  storageBucket: "inventario-ab270.firebasestorage.app",
+  messagingSenderId: "15388676345",
+  appId: "1:15388676345:web:72c8e22a2aece1d4151228"
 };
 
 firebase.initializeApp(firebaseConfig);
